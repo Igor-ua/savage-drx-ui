7857165c526ff719593a1899be37c2e091ae285e
IgorZ <694@ukr.net>
2021-02-10 15:17:06 +0200

    Python script for deployment

---
+++ tools/deploy.py
@@ -0,0 +1,40 @@
+import shutil
+from datetime import date
+from paramiko import SSHClient
+from scp import SCPClient
+
+
+class _Settings:
+    SSH_HOSTNAME = '0.0.0.0'
+    SSH_PORT = 22
+    SSH_USER = 'example'
+    SSH_PASSWORD = 'example'
+    ARCHIVE_NAME = 'ui-{}.zip'.format(date.today().strftime("%Y-%m-%d"))
+    SSH_FILE_LOCATION = '/home/igor/upload/{}'.format(ARCHIVE_NAME)
+
+
+def build_archive():
+    shutil.make_archive('./../deploy/ui-{}'.format(date.today().strftime("%Y-%m-%d")), 'zip', './../build')
+    print('Archive is ready')
+
+
+def upload_on_cloud():
+    ssh = SSHClient()
+    ssh.load_system_host_keys()
+    ssh.connect(hostname=_Settings.SSH_HOSTNAME,
+                port=_Settings.SSH_PORT,
+                username=_Settings.SSH_USER,
+                password=_Settings.SSH_PASSWORD)
+
+    scp = SCPClient(ssh.get_transport())
+
+    scp.put('./../deploy/{0}'.format(_Settings.ARCHIVE_NAME), _Settings.SSH_FILE_LOCATION)
+    # scp.get('file_path_on_remote_machine', 'file_path_on_local_machine')
+    scp.close()
+
+    print('Uploaded')
+
+
+if __name__ == '__main__':
+    build_archive()
+    upload_on_cloud()
