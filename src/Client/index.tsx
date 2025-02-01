c9f4378ceeb190f862e43a88ee11d5cce17b2446
IgorZ <694@ukr.net>
2025-02-01 17:05:52 +0200

    New client links

--- src/Client/index.tsx
+++ src/Client/index.tsx
@@ -35,15 +35,15 @@ const Client = () => {
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} content={'Download #1'}/>
                                 <Table.Cell textAlign={"left"} content={
-                                    <a href={'https://mega.nz/file/t7hnyDIL#AL1aJZNi_ZQR7XhYGSZx9vzXVnhZVuUkR4YnnPZM_n4'}>
-                                        drx_setup-1.03-cl_win_prod.exe
+                                    <a href={'https://mega.nz/file/ZvwVhB7b#z1rGg7XYKTaDgHd53JSrcZvT6QA_zryvVRyldDy_C9Y'}>
+                                        drx_setup-1.3.4-cl_win_prod.exe
                                     </a>
                                 }/>
                             </Table.Row>
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} content={'Sha256'}/>
                                 <Table.Cell textAlign={"left"} content={
-                                    '5f6fefbe29b3f1d464b23dfb85810f5ffafed98934f92378f45dc7b3819f5ed7'
+                                    'bb630e649c1e0af769e14163d670f2d3ef0f1bc71302baa8dc3ae84aaccde373'
                                 }/>
                             </Table.Row>
                         </Table.Body>
@@ -69,33 +69,41 @@ const Client = () => {
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} width={"3"} content={'Type'}/>
                                 <Table.Cell textAlign={"left"} content={
-                                    '.deb package for Debian 11/Ubuntu 22.04, 22.10 LTS/Mint/POP! OS 21.04 etc'
+                                    '.deb package for Debian/Ubuntu/Mint/PoP! OS etc'
                                 }/>
                             </Table.Row>
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} content={'Download'}/>
                                 <Table.Cell textAlign={"left"} content={
-                                    <a href={'https://mega.nz/file/1jRGTQjA#IYV_FdstUa3OJIs5rwMhO9eTY4mnyQyJ4Pp8kJbpKQQ'}>
-                                        savage-drx_2023.03.29-1.deb
+                                    <a href={'https://mega.nz/file/M7xHkILB#oxqC9Uiu9xFQVVtNUttEnYun8cs9gmH1hezAN4P65Bo'}>
+                                        savage-drx_2025.01.28-2.deb
                                     </a>
                                 }/>
                             </Table.Row>
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} content={'Sha256'}/>
                                 <Table.Cell textAlign={"left"} content={
-                                    '3b849ca419bcc837587acc4c40f6e20ab89c4032c112ce60b99036963038025b'
+                                    'dabb164c094f030d580f34e00605d73c355aa6e997408c2c85f276e566b109b5'
                                 }/>
                             </Table.Row>
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} content={'Install with'}/>
                                 <Table.Cell textAlign={"left"} content={
-                                    'sudo apt install ./savage-drx_2023.03.29-1.deb'
+                                    'sudo apt install ./savage-drx_2025.01.28-2.deb'
                                 }/>
                             </Table.Row>
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} content={'Uninstall with'}/>
                                 <Table.Cell textAlign={"left"} content={
                                     'sudo apt remove savage-drx'
+                                }/>
+                            </Table.Row>
+                            <Table.Row>
+                                <Table.Cell textAlign={"left"} content={'Wayland'}/>
+                                <Table.Cell textAlign={"left"} content={
+                                    <a href={'https://discord.com/channels/511261029838225419/1334995287604854909'}>
+                                        Guide to enabling Wayland in DRX
+                                    </a>
                                 }/>
                             </Table.Row>
                         </Table.Body>
@@ -116,15 +124,15 @@ const Client = () => {
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} content={'Download'}/>
                                 <Table.Cell textAlign={"left"} content={
-                                    <a href={'https://mega.nz/file/UmZUAS6D#sx-5X05lfHDz-_uf0IREsnmzaBuvBsrTTS0jHYOBexQ'}>
-                                        savage-drx-x86_64-2023.03.29-1.flatpak
+                                    <a href={'https://mega.nz/file/BipikKaB#Y9rGMWKpB5G2GgRNY46KyETERDmLYnsNi1hm1yDJvW8'}>
+                                        savage-drx-x86_64-2025.01.28-2.flatpak
                                     </a>
                                 }/>
                             </Table.Row>
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} content={'Sha256'}/>
                                 <Table.Cell textAlign={"left"} content={
-                                    'da222b19bf38f4a9424886fb47c57ab5d9ca22166eca9880b4cd08f4fa8294b4'
+                                    '687edeed89f0ddedc138f29dab3504434c0c3acfd28df703883380632a8ae148'
                                 }/>
                             </Table.Row>
                             <Table.Row>
@@ -133,7 +141,7 @@ const Client = () => {
                                     * flatpak --user remote-add --if-not-exists flathub
                                     https://flathub.org/repo/flathub.flatpakrepo
                                     <br/>
-                                    * flatpak --user install ./savage-drx-x86_64-2023.03.29-1.flatpak
+                                    * flatpak --user install ./savage-drx-x86_64-2025.01.28-2.flatpak
                                 </Table.Cell>
                             </Table.Row>
                             <Table.Row>
@@ -141,6 +149,16 @@ const Client = () => {
                                 <Table.Cell textAlign={"left"} content={
                                     'flatpak uninstall --user --delete-data org.savagedrx.savagedrx'
                                 }/>
+                            </Table.Row>
+                            <Table.Row>
+                                <Table.Cell textAlign={"left"} content={'Note'}/>
+                                <Table.Cell textAlign={"left"}>
+                                    Backup your cdkey and startup.cfg first if upgrading from older version, guide
+                                    here: <a
+                                    href={'https://discord.com/channels/511261029838225419/1334995287604854909'}>
+                                    discord
+                                </a>
+                                </Table.Cell>
                             </Table.Row>
                         </Table.Body>
                     </Table>
@@ -160,15 +178,15 @@ const Client = () => {
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} content={'Download'}/>
                                 <Table.Cell textAlign={"left"} content={
-                                    <a href={'https://mega.nz/file/MjBTRCQY#mnPSstwKOS7tckNh01s0MooWsIGf9jhACYbET0QpveE'}>
-                                        savage-drx_2023.03.29-1.zip
+                                    <a href={'https://mega.nz/file/JjozRZaT#kdHsO5CciHAQSqVqrIZajWawiw3eiAS9AQPQzUk65qw'}>
+                                        savage-drx_2025.01.28-2.zip
                                     </a>
                                 }/>
                             </Table.Row>
                             <Table.Row>
                                 <Table.Cell textAlign={"left"} content={'Sha256'}/>
                                 <Table.Cell textAlign={"left"} content={
-                                    '562cfa3704e307eba2e010f942153061e2e607e69dcf710d96f07d12a25a2b24'
+                                    '9517bd68f673e861fbca063a9c1a09e2ea436bacb82613de9128f4b7d51a9f00'
                                 }/>
                             </Table.Row>
                         </Table.Body>
